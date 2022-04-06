80072a120ce2801a4ed6c3b93e236a5f07d74c70
Matheus s s Busnelo <mahbusnelo@gmail.com>
2022-04-06 17:25:40 -0300

    cap 2 final

--- Fase 2/Cap 2/RM94092_ EX02.py
+++ Fase 2/Cap 2/RM94092_ EX02.py
@@ -23,3 +23,20 @@ elif(sex > seg and sex > ter and sex > qua and sex > qui):
 
 else:
     print("houve empate, votem novamente!")
+
+
+#fui pesquisar sobre listas no cap4 e acabei encontrando os dicionários para armazenar multiplas informações(k, v), decidi tentar resolver e cheguei nesta solução:
+
+##armazena os dias da semana com 0 votos para cada um
+#semana = {"segunda-feira": 0, "terça-feira": 0, "quarta-feira": 0, "quinta-feira": 0, "sexta-feira": 0}
+
+##passa por cada item de semana solicitando e atribuindo o valor do input à key(dia)
+#for dia in semana:
+#    votos = int(input("Quantos votos {} recebeu? ".format(dia)))
+#    semana[dia] = votos
+
+##busca a key(dia) com maior valor(votos) em semana
+#vencedor = max(semana, key=semana.get)
+
+##printa a key(dia) e o valor(votos) identificados em vencedor
+#print("As lives acontecerão toda {} com {} votos!".format(vencedor, semana[vencedor]))
--- Fase 2/Cap 2/RM94092_ EX04.py
+++ Fase 2/Cap 2/RM94092_ EX04.py
@@ -1,13 +1,19 @@
 #armazena os minutos
 minutos = int(input("Digite os minutos do seu computador:  "))
 
-#armazena o valor dos minutos e já subtrai 1 para o calculo dentro do loop
+#armazena o valor dos minutos e já subtrai 1 para o calculo fatorial
 fatorial = minutos - 1
 
+#confere a entrada do usuario. 0 até 59 = ok
+if(minutos > 59 or minutos < 0):
+    print("Digite um número válido para gerar a senha!")
+    exit()
+
 #multiplica os minutos pelo seu valor armazenado em fatorial -1, enquanto fatorial for > 1
-while fatorial > 1:
-    minutos = minutos * fatorial
-    fatorial -= 1
+else:    
+    while fatorial > 1:
+        minutos = minutos * fatorial
+        fatorial -= 1
 
 #exibe a senha
-print("A senha é LIBERDADE{}".format(minutos))
+print("A senha é: LIBERDADE{}".format(minutos))
